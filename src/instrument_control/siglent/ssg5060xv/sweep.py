d1f7c9053e64943a54cf97f111a919331ac2726e
Viktor Doychinov <v.doychinov@bradford.ac.uk>
2026-07-08 22:40:30 +0100

    Made a start on the sweep functionality

---
+++ src/instrument_control/siglent/ssg5060xv/sweep.py
@@ -0,0 +1,6 @@
+"""Sweep functionality for Siglent SSG5060X-V
+
+Implements various frequency and amplitude sweeps - list, step, linear, and
+logarithmic. These are all triggered internally and there is no software sync
+over the VISA connection.
+"""
